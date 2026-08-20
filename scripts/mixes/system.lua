b5c5892b830a0d6958dbc9cbfe17a23aa39b9626
tjdulka3 <134152502+tjdulka3@users.noreply.github.com>
2026-08-20 15:09:53 -0500

    lowered SPEED_FACTOR

--- scripts/mixes/system.lua
+++ scripts/mixes/system.lua
@@ -50,7 +50,7 @@ local WORK_ANGLE     = 0.50
 
 -- Steering behavior
 local TURN_GAIN    = 0.25
-local SPEED_FACTOR = 0.30
+local SPEED_FACTOR = 0.15
 
 -- Time-based output rates in channel units per second.
 --
