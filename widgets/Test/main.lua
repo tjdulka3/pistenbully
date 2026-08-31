2f60de56b7a268e83726c658f840f1b5f14ed115
tjdulka3 <134152502+tjdulka3@users.noreply.github.com>
2026-08-31 11:26:58 -0500

    guage lables

--- widgets/Test/main.lua
+++ widgets/Test/main.lua
@@ -1857,7 +1857,7 @@ local function drawAnalogGauge(
 
   lcd.drawText(
     cx,
-    cy + 5,
+    cy - 15,
     valueText,
     SMLSIZE +
     CENTER +
@@ -1866,7 +1866,7 @@ local function drawAnalogGauge(
 
   lcd.drawText(
     cx,
-    cy + 16,
+    cy + 8,
     label,
     SMLSIZE +
     CENTER +
