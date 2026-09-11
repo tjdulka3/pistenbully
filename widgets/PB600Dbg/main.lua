05d493be89e0c6a3a9feb3f128cf24f5903aa828
tjdulka3 <134152502+tjdulka3@users.noreply.github.com>
2026-09-10 20:58:51 -0500

    moved label

--- widgets/PB600Dbg/main.lua
+++ widgets/PB600Dbg/main.lua
@@ -115,6 +115,7 @@ local function draw(zone)
   lcd.drawText(x+120,y,"SD "..fmtSD(getValue("sd")),SMLSIZE)
   lcd.drawText(x+200,y,"SC "..fmtSC(getValue("sc")),SMLSIZE)
   lcd.drawText(x+280,y,"SB "..fmtSB(getValue("sb")),SMLSIZE)
+  lcd.drawText(x+610,y,"ver. " .. APP_VERSION,SMLSIZE+INVERS)
 
   local revRequest = (getValue("sd") or 0) > 500 and (getValue("thr") or 0) < -50
   local bladeTran = luaValue(SYSTEM_SLOT, 4) > 0
@@ -279,7 +280,6 @@ local function draw(zone)
     {4,"Tiller Angle","%"}
   }
 
-  lcd.drawText(x + 690, y + 325 + (ROW * 3), "ver. " .. APP_VERSION, SMLSIZE )
 
   local gx = 10
   local gy_row = gy
